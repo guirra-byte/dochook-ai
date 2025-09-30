1f822723b6b370b39c47999198b89a9f9a126f61
guirra-byte <guirramatheus1@gmail.com>
2025-09-29 23:21:53 -0300

    fix: just renaming rabbitmq queues

--- src/modules/documents/subscribers/dowload-documents.subscriber.ts
+++ src/modules/documents/subscribers/dowload-documents.subscriber.ts
@@ -2,11 +2,11 @@ import RabbitMqProvider from "../../../core/providers/rabbitmq/index.js";
 
 import type { ConsumeMessage } from "amqplib";
 import type { DocumentsUploadedRequest } from "../http/documents.controller.js";
-import { downloadS3Docs } from "../usecases/services/download-s3-doc.js";
+import { downloadS3Docs } from "../usecases/download-s3-doc.js";
 
 async function downloadDocumentsSubscriber() {
   const rabbitMq = await RabbitMqProvider.instance();
-  rabbitMq.consume("download_docs", async (msg: ConsumeMessage | null) => {
+  rabbitMq.consume("download_new_docs", async (msg: ConsumeMessage | null) => {
     if (msg) {
       const { documents, ...contextData } = JSON.parse(
         msg.content.toString("utf-8")
